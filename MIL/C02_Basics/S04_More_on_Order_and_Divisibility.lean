181db5261af7961cadd74d6d779df79f75a216f6
Hideaki Kawai <kayhide@gmail.com>
2025-05-25 22:13:08 +0900

    Done 2.4

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,72 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  have h : ∀ x y : ℝ, max x y ≤ max y x := by
+    intro x y
+    apply max_le
+    . apply le_max_right
+    . apply le_max_left
+  apply le_antisymm
+  repeat
+    apply h
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  have h₁ : ∀ x y z : ℝ, min (min x y) z = min (min x y) (min y z) := by
+    intro x y z
+    apply le_antisymm
+    . show min (min x y) z ≤ min (min x y) (min y z)
+      apply le_min
+      . show min (min x y) z ≤ min x y
+        apply min_le_left
+      . show min (min x y) z ≤ min y z
+        apply le_min
+        . apply le_trans
+          . show min (min x y) z ≤ min x y
+            apply min_le_left
+          . show min x y ≤ y
+            apply min_le_right
+        . apply min_le_right
+    . apply le_min
+      . apply min_le_left
+      . apply le_trans
+        . apply min_le_right
+        . apply min_le_right
+
+  have h₂ : min a (min b c) = min (min a b) (min b c) := by
+    rw [min_comm a, min_comm (min a b), min_comm a b, min_comm b c, h₁]
+  rw [h₁, h₂]
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  . apply add_le_add_right
+    apply min_le_left
+  . apply add_le_add_right
+    apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  . apply aux
+  . calc
+      min (a + c) (b + c) = min (a + c) (b + c) - c + c := by rw [sub_add_cancel]
+      _ ≤ min a b + c := by
+        apply add_le_add_right
+        apply le_min
+        . nth_rw 2 [← add_sub_cancel_right a c]
+          apply sub_le_sub_right
+          apply min_le_left
+        . nth_rw 2 [← add_sub_cancel_right b c]
+          apply sub_le_sub_right
+          apply min_le_right
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
 example : |a| - |b| ≤ |a - b| :=
-  sorry
+  calc
+    |a| - |b| = |a - b + b| - |b| := by rw [sub_add_cancel]
+    _ ≤ |a - b| + |b| - |b| := by
+      apply sub_le_sub_right
+      apply abs_add
+    _ = |a - b| := by ring
 end
 
 section
@@ -66,7 +121,13 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  . have h₁ : y * (x * z) + x ^ 2  = x * (y * z + x) := by ring
+    rw [h₁]
+    apply dvd_mul_right
+  . apply dvd_trans
+    . exact h
+    . apply dvd_mul_left
 end
 
 section
@@ -78,7 +139,9 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply dvd_antisymm
+  repeat
+    apply dvd_gcd
+    . apply gcd_dvd_right
+    . apply gcd_dvd_left
 end
-
-
