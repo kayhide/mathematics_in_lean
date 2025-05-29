c7dba9b9a627d727f118f53b527a4aea5eaf58dd
Hideaki Kawai <kayhide@gmail.com>
2025-05-29 10:20:44 +0900

    Done 3.4

--- MIL/C03_Logic/S04_Conjunction_and_Iff.lean
+++ MIL/C03_Logic/S04_Conjunction_and_Iff.lean
@@ -63,8 +63,10 @@ example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
 example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x :=
   fun h' ↦ h.right (le_antisymm h.left h')
 
-example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m :=
-  sorry
+example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m := by
+  obtain ⟨mdvdn, h'⟩ := h
+  contrapose! h'
+  exact dvd_antisymm mdvdn (h' mdvdn)
 
 example : ∃ x : ℝ, 2 < x ∧ x < 4 :=
   ⟨5 / 2, by norm_num, by norm_num⟩
@@ -101,15 +103,30 @@ example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y := by
 example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y :=
   ⟨fun h₀ h₁ ↦ h₀ (by rw [h₁]), fun h₀ h₁ ↦ h₀ (le_antisymm h h₁)⟩
 
-example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y :=
-  sorry
+example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y := by
+  constructor
+  . intro ⟨xley, h⟩
+    contrapose! h
+    have : x = y := h xley
+    rw [this]
+  . intro ⟨xley, h⟩
+    contrapose! h
+    exact le_antisymm xley (h xley)
 
 theorem aux {x y : ℝ} (h : x ^ 2 + y ^ 2 = 0) : x = 0 :=
-  have h' : x ^ 2 = 0 := by sorry
+  have h' : x ^ 2 = 0 := by
+    linarith [pow_two_nonneg x, pow_two_nonneg y]
   pow_eq_zero h'
 
-example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 :=
-  sorry
+example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 := by
+  constructor
+  . intro h
+    constructor
+    . exact aux h
+    . rw [add_comm] at h
+      exact aux h
+  . rintro ⟨rfl, rfl⟩
+    norm_num
 
 section
 
@@ -130,7 +147,10 @@ theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y �
   rfl
 
 example : ¬Monotone fun x : ℝ ↦ -x := by
-  sorry
+  rw [Monotone]
+  push_neg
+  use 0, 1
+  norm_num
 
 section
 variable {α : Type*} [PartialOrder α]
@@ -138,7 +158,13 @@ variable (a b : α)
 
 example : a < b ↔ a ≤ b ∧ a ≠ b := by
   rw [lt_iff_le_not_le]
-  sorry
+  constructor
+  . rintro ⟨aleb, h⟩
+    contrapose! h
+    rw [h aleb]
+  . rintro ⟨aleb, h⟩
+    contrapose! h
+    exact le_antisymm aleb (h aleb)
 
 end
 
@@ -148,10 +174,17 @@ variable (a b c : α)
 
 example : ¬a < a := by
   rw [lt_iff_le_not_le]
-  sorry
+  push_neg
+  intro alea
+  exact alea
 
 example : a < b → b < c → a < c := by
   simp only [lt_iff_le_not_le]
-  sorry
+  rintro ⟨aleb, h⟩ ⟨blec, _⟩
+  constructor
+  . exact le_trans aleb blec
+  . intro clea
+    apply h
+    exact le_trans blec clea
 
 end
