8e89d60416496753941397fc337911df343a2a10
Hideaki Kawai <kayhide@gmail.com>
2025-06-18 22:43:57 +0900

    Done 4.3

--- MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
+++ MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
@@ -28,10 +28,16 @@ theorem sb_right_inv {x : α} (hx : x ∉ sbSet f g) : g (invFun g x) = x := by
     rw [sbSet, mem_iUnion]
     use 0
     rw [sbAux, mem_diff]
-    sorry
+    constructor
+    . trivial
+    . assumption
   have : ∃ y, g y = x := by
-    sorry
-  sorry
+    use sbFun f g x
+    rw [sbFun, if_neg hx, invFun]
+    simp at this
+    rw [dif_pos this]
+    exact choose_spec this
+  exact invFun_eq this
 
 theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -50,15 +56,21 @@ theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
       rw [if_pos x₁A, if_neg x₂nA] at hxeq
       rw [A_def, sbSet, mem_iUnion] at x₁A
       have x₂eq : x₂ = g (f x₁) := by
-        sorry
+        rw [A_def] at x₂nA
+        rw [hxeq, sb_right_inv f g x₂nA]
       rcases x₁A with ⟨n, hn⟩
       rw [A_def, sbSet, mem_iUnion]
       use n + 1
       simp [sbAux]
       exact ⟨x₁, hn, x₂eq.symm⟩
-    sorry
+    rw [if_pos x₁A, if_pos x₂A] at hxeq
+    exact hf hxeq
   push_neg at xA
-  sorry
+  rcases xA with ⟨x₁nA, x₂nA⟩
+  rw [if_neg x₁nA, if_neg x₂nA] at hxeq
+  have : g (invFun g x₁) = g (invFun g x₂) := congrArg g hxeq
+  rw [sb_right_inv f g x₁nA, sb_right_inv f g x₂nA] at this
+  assumption
 
 theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -77,8 +89,9 @@ theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
       exact ⟨n, xmem⟩
     rw [h_def, sbFun, if_pos this]
     apply hg hx
-
-  sorry
+  use g y
+  rw [h_def, sbFun, if_neg gyA]
+  apply leftInverse_invFun hg
 
 end
 
