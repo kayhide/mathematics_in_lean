db053139f1411dfd0db5c1e2e9dc7b95db9deadd
Hideaki Kawai <kayhide@gmail.com>
2025-07-27 16:41:41 +0100

    Done 7.2

--- MIL/C07_Structures/S02_Algebraic_Structures.lean
+++ MIL/C07_Structures/S02_Algebraic_Structures.lean
@@ -54,8 +54,14 @@ def permGroup {α : Type*} : Group₁ (Equiv.Perm α)
   inv_mul_cancel := Equiv.self_trans_symm
 
 structure AddGroup₁ (α : Type*) where
-  (add : α → α → α)
-  -- fill in the rest
+  add : α → α → α
+  zero : α
+  neg : α → α
+  add_assoc {x y z : α} : add x (add y z) = add (add x y) z
+  zero_add {x : α} : add zero x = x
+  add_zero {x : α} : add x zero = x
+  neg_add_cancel {x : α} : add (neg x) x = zero
+
 @[ext]
 structure Point where
   x : ℝ
@@ -67,11 +73,22 @@ namespace Point
 def add (a b : Point) : Point :=
   ⟨a.x + b.x, a.y + b.y, a.z + b.z⟩
 
-def neg (a : Point) : Point := sorry
+def neg (a : Point) : Point := ⟨- a.x, - a.y, - a.z⟩
 
-def zero : Point := sorry
+def zero : Point := ⟨0, 0, 0⟩
 
-def addGroupPoint : AddGroup₁ Point := sorry
+def addGroupPoint : AddGroup₁ Point where
+  add := add
+  zero := zero
+  neg := neg
+  add_assoc := by
+    simp [Point.add, add_assoc]
+  zero_add := by
+    simp [Point.add, Point.zero]
+  add_zero := by
+    simp [Point.add, Point.zero]
+  neg_add_cancel := by
+    simp [Point.add, Point.neg, Point.zero]
 
 end Point
 
@@ -170,4 +187,24 @@ end
 
 class AddGroup₂ (α : Type*) where
   add : α → α → α
-  -- fill in the rest
+  zero : α
+  neg : α → α
+  add_assoc {x y z : α} : add x (add y z) = add (add x y) z
+  zero_add {x : α} : add zero x = x
+  add_zero {x : α} : add x zero = x
+  neg_add_cancel {x : α} : add (neg x) x = zero
+
+instance {α : Type*} [AddGroup₂ α] : Add α := ⟨AddGroup₂.add⟩
+
+instance {α : Type*} [AddGroup₂ α] : Zero α := ⟨AddGroup₂.zero⟩
+
+instance {α : Type*} [AddGroup₂ α] : Neg α := ⟨AddGroup₂.neg⟩
+
+instance : AddGroup₂ Point where
+  add := Point.add
+  zero := Point.zero
+  neg := Point.neg
+  add_assoc := by simp [Point.add, add_assoc]
+  zero_add := by simp [Point.add, Point.zero, zero_add]
+  add_zero := by simp [Point.add, Point.zero, add_zero]
+  neg_add_cancel := by simp [Point.add, Point.zero, Point.neg]
