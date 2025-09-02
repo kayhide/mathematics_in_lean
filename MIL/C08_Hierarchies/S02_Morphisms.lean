9f2720d59503eb82393c0c49d9a194f19cc3d4b0
Hideaki Kawai <kayhide@gmail.com>
2025-09-02 14:02:50 +0900

    Done 8.2

--- MIL/C08_Hierarchies/S02_Morphisms.lean
+++ MIL/C08_Hierarchies/S02_Morphisms.lean
@@ -105,13 +105,24 @@ structure OrderPresHom (α β : Type) [LE α] [LE β] where
 structure OrderPresMonoidHom (M N : Type) [Monoid M] [LE M] [Monoid N] [LE N] extends
 MonoidHom₁ M N, OrderPresHom M N
 
-class OrderPresHomClass (F : Type) (α β : outParam Type) [LE α] [LE β]
+class OrderPresHomClass (F : Type) (α β : outParam Type) [LE α] [LE β] extends
+    DFunLike F α (fun _ ↦ β) where
+  le_of_le : ∀ (f : F) a a', a ≤ a' → f a ≤ f a'
 
 instance (α β : Type) [LE α] [LE β] : OrderPresHomClass (OrderPresHom α β) α β where
+  coe := OrderPresHom.toFun
+  coe_injective' _ _ := OrderPresHom.ext
+  le_of_le := OrderPresHom.le_of_le
 
 instance (α β : Type) [LE α] [Monoid α] [LE β] [Monoid β] :
     OrderPresHomClass (OrderPresMonoidHom α β) α β where
+  coe f := f.toOrderPresHom.toFun
+  coe_injective' _ _ := OrderPresMonoidHom.ext
+  le_of_le f := f.toOrderPresHom.le_of_le
 
 instance (α β : Type) [LE α] [Monoid α] [LE β] [Monoid β] :
-    MonoidHomClass₃ (OrderPresMonoidHom α β) α β
-  := sorry
+    MonoidHomClass₃ (OrderPresMonoidHom α β) α β where
+  coe f := f.toMonoidHom₁.toFun
+  coe_injective' _ _ := OrderPresMonoidHom.ext
+  map_one f := f.toMonoidHom₁.map_one
+  map_mul f := f.toMonoidHom₁.map_mul
