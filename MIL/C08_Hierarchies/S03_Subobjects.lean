5ab95c043b779e3d15a84def079d75013f6407b4
Hideaki Kawai <kayhide@gmail.com>
2025-09-03 09:34:05 +0900

    Done 8.3

--- MIL/C08_Hierarchies/S03_Subobjects.lean
+++ MIL/C08_Hierarchies/S03_Subobjects.lean
@@ -53,6 +53,25 @@ instance [Monoid M] : SubmonoidClass₁ (Submonoid₁ M) M where
   one_mem := Submonoid₁.one_mem
 
 
+@[ext]
+structure Subgroup₁ (G : Type) [Group G] extends Submonoid₁ G where
+  inv_mem : a ∈ carrier → a⁻¹ ∈ carrier
+
+instance [Group G] : SetLike (Subgroup₁ G) G where
+  coe x := x.carrier
+  coe_injective' _ _ := Subgroup₁.ext
+
+instance [Group G] (H : Subgroup₁ G): Group H where
+  mul := fun ⟨x, xh⟩ ⟨y, yh⟩ ↦ ⟨x * y, H.mul_mem xh yh⟩
+  mul_assoc := fun ⟨x, _⟩ ⟨y, _⟩ ⟨z, _⟩ ↦ SetCoe.ext (mul_assoc x y z)
+  one := ⟨1, H.one_mem⟩
+  one_mul := fun ⟨x, _⟩ ↦ SetCoe.ext (one_mul x)
+  mul_one := fun ⟨x, _⟩ ↦ SetCoe.ext (mul_one x)
+  inv := fun ⟨x, xh⟩ ↦ ⟨x⁻¹, H.inv_mem xh⟩
+  inv_mul_cancel := fun ⟨x, _⟩ ↦ SetCoe.ext (inv_mul_cancel x)
+
+
+
 instance [Monoid M] : Min (Submonoid₁ M) :=
   ⟨fun S₁ S₂ ↦
     { carrier := S₁ ∩ S₂
@@ -69,7 +88,14 @@ def Submonoid.Setoid [CommMonoid M] (N : Submonoid M) : Setoid M  where
     refl := fun x ↦ ⟨1, N.one_mem, 1, N.one_mem, rfl⟩
     symm := fun ⟨w, hw, z, hz, h⟩ ↦ ⟨z, hz, w, hw, h.symm⟩
     trans := by
-      sorry
+      intro x y z
+      rintro ⟨a, ha, b, hb, h₁⟩
+      rintro ⟨c, hc, d, hd, h₂⟩
+      use (a * c), mul_mem ha hc
+      use (d * b), mul_mem hd hb
+      rw [← mul_assoc x, h₁]
+      rw [← mul_assoc z, ← h₂]
+      rw [mul_assoc, mul_comm b c, mul_assoc]
   }
 
 instance [CommMonoid M] : HasQuotient M (Submonoid M) where
@@ -79,12 +105,31 @@ def QuotientMonoid.mk [CommMonoid M] (N : Submonoid M) : M → M ⧸ N := Quotie
 
 instance [CommMonoid M] (N : Submonoid M) : Monoid (M ⧸ N) where
   mul := Quotient.map₂ (· * ·) (by
-      sorry
+      rintro a b ⟨x, hx, y, hy, h₁⟩ c d ⟨z, hz, w, hw, h₂⟩
+      simp
+      use (x * z), mul_mem hx hz
+      use (y * w), mul_mem hy hw
+      calc
+        (a * c) * (x * z) = a * (c * x) * z := by repeat rw [mul_assoc]
+        _ = a * (x * c) * z := by rw [mul_comm c x]
+        _ = (a * x) * (c * z) := by repeat rw [mul_assoc]
+        _ = (b * y) * (d * w) := by rw [h₁, h₂]
+        _ = b * (y * d) * w := by repeat rw [mul_assoc]
+        _ = b * (d * y) * w := by rw [mul_comm y d]
+        _ = b * d * (y * w) := by repeat rw [mul_assoc]
         )
   mul_assoc := by
-      sorry
+      rintro ⟨a⟩ ⟨b⟩ ⟨c⟩
+      apply Quotient.sound
+      simp
+      rw [mul_assoc]
+      apply @Setoid.refl M N.Setoid
   one := QuotientMonoid.mk N 1
   one_mul := by
-      sorry
+      rintro ⟨a⟩
+      apply Quotient.sound
+      simp
   mul_one := by
-      sorry
+      rintro ⟨_⟩
+      apply Quotient.sound
+      simp
